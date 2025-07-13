396e61fddf0da019e5fa3a9f7ee88e95ae21720c
Marcos Juárez Agüero <66289881+MarcosPrograma@users.noreply.github.com>
2025-07-13 18:40:08 -0300

    Suavizar el movimiento

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 const mindarThree = new MindARThree({
     container: document.querySelector("#container"),
         imageTargetSrc: "https://cdn.glitch.global/65e71ed9-5bd3-4f28-832d-b9b8da88b976/targets.mind?v=1748745952253",
-        filterMinCF: 0.3,
-        filterBeta: 1,
+        filterMinCF: 0.5,
+        filterBeta: 0.1,
         warmupTolerance: 5,
         missTolerance: 5,
     });
@@ -25,9 +25,9 @@ loader.load("https://cdn.glitch.global/65e71ed9-5bd3-4f28-832d-b9b8da88b976/davi
         model.scale.set(0.25, 0.25, 0.25);
         model.rotation.set(THREE.MathUtils.degToRad(90), 0, 0);
         model.position.set(0, 0, 0.1);
-        anchor.group.add(model);
-        }
-    );
+        anchor.group.add(model);      
+    }
+);
 
 // Iniciar AR
 const start = async () => {
@@ -35,36 +35,6 @@ const start = async () => {
     renderer.setAnimationLoop(() => {
         renderer.render(scene, camera);
     });
-};
-
-let modelAdded = false;
-let savedModel = null;
-
-anchor.onTargetFound = () => {
-  if (modelAdded) return;
-
-  // Clonar el modelo
-  savedModel = anchor.group.children[0].clone();
-
-  // Obtener posición y rotación absolutas del anchor
-  anchor.group.updateWorldMatrix(true, false);
-  const worldPosition = new THREE.Vector3();
-  const worldQuaternion = new THREE.Quaternion();
-  const worldScale = new THREE.Vector3();
-  anchor.group.matrixWorld.decompose(worldPosition, worldQuaternion, worldScale);
-
-  // Aplicar transformaciones al modelo clonado
-  savedModel.position.copy(worldPosition);
-  savedModel.quaternion.copy(worldQuaternion);
-  savedModel.scale.copy(worldScale);
-
-  // Agregarlo a la escena principal
-  scene.add(savedModel);
-
-  // Eliminar el modelo original (opcional)
-  //anchor.group.clear();
-
-  modelAdded = true;
 };
 
 start();
